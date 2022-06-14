59f9f92cbb2535e10c4e5af64b30c48cab5fb4c1
vitalspace <myglobalsounds@gmail.com>
2022-06-14 21:27:44 +0000

    Added classes on frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import cors from "cors"
+import http from "http";
+import express from "express"
+import geckos  from "@geckos.io/server"
+
+const app = express();
+const App = {
+	app: app,
+	port: 4000,
+	statics: "www",
+	server: http.createServer(app),
+	io: geckos(),
+	message: "Server on port on",
+	Init: () => {
+		App.Middleware()
+		App.Geckos()
+		App.CreateServer()
+	},
+	Middleware: () => {
+		App.app.use(cors());
+		App.app.use(express.json());
+		App.app.use(express.static(App.statics));
+		App.app.use(express.urlencoded({ extended: true }));
+	},
+	Geckos: () => {
+		App.io.addServer(App.server)
+		App.io.onConnection((channel) => {
+			console.log("New Connection", channel.id);
+			channel.on('helloServer', (data) => {
+				console.log(data);
+			})
+		})
+	},
+	CreateServer: () => {
+		App.server.listen(App.port, () => {
+			console.log(App.message, App.port);
+		})
+	}
+}
+
+App.Init();
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,23 @@
 import './style.css'
+import geckos from "@geckos.io/client";
 
 const app = document.querySelector<HTMLDivElement>('#app')!
+
+const App = {
+  channel : geckos({ url: "http:localhost:4000", port: undefined }),
+  init: () => {
+    App.CreateConnection();
+    App.HelloServer();
+  },
+  CreateConnection:() => {
+    App.channel.onConnect((error) => console.log(error))
+  },
+  HelloServer: () => {
+    App.channel.emit('helloServer', `Hello Server i'm ${App.channel.id}`);
+  }
+}
+
+App.init()
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
